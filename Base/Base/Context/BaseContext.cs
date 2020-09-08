f39b315c94499e1130cbab43a17962d1e61b27f7
Tiago Alves <tiago.farias@dcomp.ufs.br>
2020-09-07 22:36:40 -0300

    Add Project

---
+++ Base/Base/Context/BaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Base.Models;
+
+namespace Base.Context
+{
+    public class BaseContext : DbContext
+    {
+        public BaseContext()
+            :base("BaseContext")
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
